b89d9e2a29f10cfeb214942cd73561ade9fe2e85
bantawa04 <rahunn3@gmail.com>
2022-09-09 12:45:43 +0545

    14.27 prepairing for image upload

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/widgets/image_picker.dart';
+import 'package:flutter_chat/widgets/user_image_picker.dart';
+import 'dart:io';
 
 class AuthForm extends StatefulWidget {
   final bool isLoading;
@@ -26,10 +27,27 @@ class _AuthFormState extends State<AuthForm> {
   var _userEmail = '';
   var _userName = '';
   var _password = '';
+  File? _userImageFile;
+
+  void _pickedImage(File image) {
+    _userImageFile = image;
+  }
 
   void _trySumbit() {
     final isValid = _formKey.currentState!.validate();
     FocusScope.of(context).unfocus();
+
+    if (_userImageFile == null && !isLogin) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text("Please pick an image"),
+          duration: const Duration(seconds: 2),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+      return;
+    }
+
     if (isValid) {
       _formKey.currentState!.save();
       widget.submitForm(
@@ -62,7 +80,7 @@ class _AuthFormState extends State<AuthForm> {
             child: Column(
               mainAxisSize: MainAxisSize.min, //take only space as it needs
               children: <Widget>[
-                if (!isLogin) const UserImagePicker(),
+                if (!isLogin) UserImagePicker(_pickedImage),
                 TextFormField(
                   key: const ValueKey('email'),
                   decoration: const InputDecoration(
